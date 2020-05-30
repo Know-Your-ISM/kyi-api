feb6f87ff56cc5237d24c797f078bceca28ba308
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-05-30 12:44:13 +0530

    added defaults for StuentSchema in BTech and Mtech

--- models/BTech.js
+++ models/BTech.js
@@ -2,6 +2,12 @@ const mongoose = require ('mongoose');
 
 var StudentSchema = require ("./Student");
 
+StudentSchema.course = {
+    type: String,
+    default: "BTech",
+    trim: true
+};
+
 // StudentSchema.index({ Name: 1, Sex: 1, Place: 1, Branch: 1 });
 
 module.exports = mongoose.model("BTech", StudentSchema);
