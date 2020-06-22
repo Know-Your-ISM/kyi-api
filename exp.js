ce6fb84ff720aedfccb252d894561d04650275ed
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-06-22 12:47:44 +0530

    domain specific cors

--- exp.js
+++ exp.js
@@ -14,7 +14,7 @@ require ("./db/mongoose");
 exp.use('/views', express.static('views'));
 
 exp.use(function (req, res, next){
-	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Origin", "https://kyi.herokuapp.com, https://kyism.ga");
 	res.header("Access-Control-Allow-Methods", "*");
 	res.header("Access-Control-Allow-Headers", "Authorization, Accept, Content-Type, Origin, X-Requested-With");
 	next();
