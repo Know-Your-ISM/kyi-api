d0b91223bedf7dfc78cb6d7d46fdda11d6ae5bd3
Mihir Jichkar <j.mihir19.mj@gmail.com>
2020-06-25 23:13:09 +0530

    cors for all

--- exp.js
+++ exp.js
@@ -15,11 +15,11 @@ require ("./db/mongoose");
 exp.use('/views', express.static('views'));
 
 exp.use(function (req, res, next){
-	let index = allowed_origins.findIndex(origin => {
-		return req.headers.origin == origin ? true : false;
-	});
-	if (index === -1) index = 1;
-	res.header("Access-Control-Allow-Origin", allowed_origins[index]);
+	// let index = allowed_origins.findIndex(origin => {
+	// 	return req.headers.origin == origin ? true : false;
+	// });
+	// if (index === -1) index = 0;
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET, ");
 	res.header("Access-Control-Allow-Headers", "Authorization, Accept, Content-Type, Origin, X-Requested-With");
 	next();
